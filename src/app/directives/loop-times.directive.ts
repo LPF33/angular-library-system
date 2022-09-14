b386ed80963dce728c74b7d810941f1354eb95cd
lfeigl <larspefe@gmail.com>
2022-09-14 19:26:55 +0200

    feat(random color): custom directive with random color per loop

--- src/app/directives/loop-times.directive.ts
+++ src/app/directives/loop-times.directive.ts
@@ -7,12 +7,17 @@ export class LoopTimesDirective {
 
   constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<any>) { }
 
-  @Input("appLoopTimes") set length(num: number){
+  @Input("appLoopTimes") set length(num: number) {
     this.viewContainer.clear();
-    for( let i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
       this.viewContainer.createEmbeddedView(this.templateRef, {
-        index: i
+        index: i,
+        color: `rgb(${this.getRandomRGBValue()},${this.getRandomRGBValue()},${this.getRandomRGBValue()})`,
       })
     }
+  }
+
+  getRandomRGBValue() {
+    return Math.floor(Math.random() * 256);
   }
 }
